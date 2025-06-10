f7a6f6df4fb514ad637011c962c8a4286350dd01
L. Linders <22113355+llinders@users.noreply.github.com>
2025-06-10 18:06:00 +0200

    implement running searches without ReAct agent for more efficient design

--- agents/search_agent.py
+++ agents/search_agent.py
@@ -1,5 +1,5 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 from langgraph.constants import END
 from langgraph.prebuilt import create_react_agent
@@ -8,10 +8,9 @@ from langgraph.types import Command
 from core.llm_config import llm
 from core.state import State
 
+search_agent_tool = TavilySearch(max_results=5)
 
-search_tool = TavilySearch(max_results=5)
-
-search_agent = create_react_agent(llm, tools=[search_tool])
+search_agent = create_react_agent(llm, tools=[search_agent_tool])
 
 def search_node(state: State) -> Command:
     result = search_agent.invoke(state)
@@ -24,11 +23,18 @@ def search_node(state: State) -> Command:
         goto=END
     )
 
+def run_search_queries(state: State) -> dict:
+    """
+    Runs the search queries found in the state and returns an update dictionary.
+    """
+    search_results_tool = TavilySearchResults(max_results=4)
+    queries = state["search_queries"]
+    print(f"Running {len(queries)} search queries...")
+    all_results = []
+    for query in queries:
+        print(f"  - Searching for: '{query}'")
+        results = search_results_tool.invoke(query)
+        all_results.extend(results)
 
-def search_node_test():
-    state = {
-        "messages": [HumanMessage(content="Explain the current state of climate change debates.")],
-        "search_queries": ["Test search query"],
-    }
-    result = search_agent.invoke(state)
-    return result["messages"][-1].content
+    print(f"Successfully retrieved {len(all_results)} articles from the web.")
+    return {"raw_articles": all_results}
